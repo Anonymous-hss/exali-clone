73805c40f2b940e47d1f044db7427fe303b1de05
Anonymous-hss <harshalsawatkar24@gmail.com>
2025-07-22 23:17:22 +0530

    wrote business logic

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,5 +1,28 @@
-import express from "express";
+import express, { json } from "express";
+import jwt from "jsonwebtoken";
 
 const app = express();
+
+app.post("./signup", (req, res) => {});
+
+app.post("./signin", (req, res) => {
+  const userId = 1;
+  const token = jwt.sign(
+    {
+      userId,
+    },
+    JWT_SECRET
+  );
+
+  res.json({
+    json,
+  });
+});
+
+app.post("./room", (req, res) => {
+  res.json({
+    roomId: 123,
+  });
+});
 
 app.listen(3001);
--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -1,7 +1,22 @@
 import { WebSocketServer } from "ws";
+import jwt, { JwtPayload } from "jsonwebtoken";
 
 const ws = new WebSocketServer({ port: 8080 });
-ws.on("connection", function connection(ws) {
+ws.on("connection", function connection(ws, request) {
+  const url = request.url;
+  if (url) {
+    return;
+  }
+
+  const queryParam = new URLSearchParams(url?.split("?")[1]);
+  const token = queryParam.get("token");
+  const decoded = jwt.verify(token, JWT_SECRET);
+
+  if (!decoded || !(decoded as JwtPayload).userId) {
+    ws.close();
+    return;
+  }
+
   ws.on("message", function message(data) {
     ws.send("pong");
   });
